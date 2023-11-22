59f112085bdd60fd0b7a0ce2debf6e4bec90afa5
Daniel Vingo <dan@danvingo.com>
2023-11-22 11:08:06 -0500

    add tanstack/react-query

--- src/browser/main.cljs
+++ src/browser/main.cljs
@@ -2,12 +2,15 @@
   (:require
    ["@js-joda/core" :refer [ZonedDateTime] :as js-joda]
    ["@js-joda/timezone/dist/js-joda-timezone-10-year-range"]
-   ["@js-joda/locale_en-us" :as locale]))
+   ["@js-joda/locale_en-us" :as locale]
+   ["@tanstack/react-query" :as rc :refer[QueryClient]]))
 
 ;; (js/console.log " joda locale: " Locale)
 (def us-locale (. locale/Locale -US))
 (js/console.log "US locale:" us-locale)
 (js/console.log "today: " (. ZonedDateTime now))
+(js/console.log "rc: " rc)
+(js/console.log "QueryClient: " QueryClient)
 
 (defn add-date []
   (let [zdt (.of ZonedDateTime 2016 1 1 0 0 0 0 (.of js-joda/ZoneId "Europe/Berlin"))
